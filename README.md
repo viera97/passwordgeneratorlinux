4fb50774f5793b0b00b7aa9df276f280930a986b
Viera97 <d.viera1997@gmail.com>
2023-10-12 14:55:16 -0400

    Added files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # passwordgeneratorlinux
-Espanso extension for generating passwords and copy it to cliboard
+[Espanso](https://espanso.org) extension for generating passwords. For now this extension is only supported on unix systems, beacause of the difference of path between `dos` an `unix` systems. If your are looking this extension for `windows` go to [passwordgeneratorwin](https://github.com/viera97/passwordgeneratorwin).
+## Installation
+```bash
+espanso install passwordgeneratorlinux --git https://github.com/viera97/passwordgeneratorlinux --external
+```
+## Usage
+
+* By typing `:genpass:` a 12 length password will be generated containing lower and upper characters, symbols and digits.
+* By typing `:genpass(n)` where $n>4$ is an integer number you can generate a $n$-length password containing lower and upper characters, symbols and digits.
---
+++ password_generator.py
@@ -0,0 +1,54 @@
+import random
+import os
+import string
+
+def check_pass(password):
+    cont = 0
+    if password.lower() == password or password.upper() == password:
+        return False
+    else:
+        cont += 1
+
+    for character in password:
+        if character in characters[1]:
+            cont += 1
+            break
+    
+    for character in password:
+        if character in characters[2]:
+            cont += 1
+            break
+
+    if cont == 3:
+        return True
+    else:
+        return False
+    
+def generate(n):
+    password = ''
+    for i in range(n):
+        password += random.choice(random.choice(characters))
+    if check_pass(password):
+        return password
+    else:
+       return generate(n)
+
+try:
+    myvar = os.environ['ESPANSO_MYVAR']
+    mybarbool = True
+except:
+    mybarbool = False
+
+characters = [list(string.ascii_letters), list(string.punctuation), list(string.digits)]
+
+if mybarbool:
+    try:
+        myvar = int(myvar)
+        password = generate(myvar)
+        print(password)
+    except:
+        password = generate(12)
+        print(password)
+else:
+    password = generate(12)
+    print(password)
